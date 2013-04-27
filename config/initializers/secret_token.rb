6caeb718903ed5e1f77bfaf0624eaa056f409e50
Matias Anaya <matiasanaya@gmail.com>
2013-04-27 14:35:54 -0400

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webofcards::Application.config.secret_token = '76f12a677d21b9c49e51b75636494b9eeafdcc73caf3c9e60c355bca62111be19314aaa57a0cbf69b398de6f112aab8a5d7c7214f48304feb368a06a62f73724'
